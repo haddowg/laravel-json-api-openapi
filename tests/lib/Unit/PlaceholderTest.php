f6b6c747a7254036f64c32869ec95933f9be3d3a
Gregory Haddow <gregory.haddow@chatloop.com>
2025-01-31 15:56:34 +0000

    ci: fix run-tests

---
+++ tests/lib/Unit/PlaceholderTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace haddowg\JsonApiOpenApi\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class PlaceholderTest extends TestCase
+{
+    public function testPlaceholder()
+    {
+        $this->assertTrue(true);
+    }
+}
